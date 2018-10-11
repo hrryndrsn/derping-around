efc07a018fc64bf2c92b4914f11c98c990210645
hrryndrsn <harryanderson9@gmail.com>
2018-10-11 22:08:35 +1100

    This is a new commit for what I originally planned to be amended

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Derping Around</h1>
+    <p>Complete coursres about the best tools and design systems. Protype and build apps with React and Swift.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage

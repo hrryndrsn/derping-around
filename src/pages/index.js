909c2f83b2ac6294e0a5317dc8e0217b517914e5
hrryndrsn <harryanderson9@gmail.com>
2018-10-11 22:10:41 +1100

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Derping Around</h1>
     <p>Complete coursres about the best tools and design systems. Protype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video to see more</Link>
   </Layout>
 )
 

b64c2158e69af3855cb4b0f32b0b3be30276eb92
hrryndrsn <harryanderson9@gmail.com>
2018-10-12 20:20:05 +1100

    basic css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,16 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Derping Around</h1>
-    <p>Complete coursres about the best tools and design systems. Protype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Watch the video to see more</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Derping Around</h1>
+        <p>
+          Complete coursres about the best tools and design systems. Protype and
+          build apps with React and Swift.
+        </p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
